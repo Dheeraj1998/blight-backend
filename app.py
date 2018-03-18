d734ae479c8442e73da4b7de8406ab7f2fe50a0e
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2018-03-19 03:31:35 +0530

    Added a scheduler function and some bug fixes.

--- app.py
+++ app.py
@@ -3,8 +3,29 @@
 import json
 import requests
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, request
 from pyfcm import FCMNotification
+
+# Defining the server key
+push_service = FCMNotification(api_key="AAAAI6Yezgk:APA91bHTgkp5RKolA8Fb2VzYZjCt5z8iadT6dmRUzAVhzqWngXiFtHLu__spT82XXXy9vstzBixnzuG0Rgtuyf6hsCBywd6fpYXpsI5Gma6t9vfdyIkEA9b4UtBc4SRg1w6IBmKmwBFk")
+
+# Function for checking any disaster
+def check_disaster():
+	api_key = '3rRLEdKYJZYfSA0nvMftbmf4wJSUzL8J'
+	disaster_url = 'http://dataservice.accuweather.com/alarms/v1/1day/190975?apikey=' + api_key
+
+	disaster_response = requests.get(disaster_url)
+	
+	if (disaster_response.text != '[]'):
+		print('There is an emergency.')
+		
+	return len(disaster_response.text)
+	
+# Starting the scheduler with the required job
+task_scheduler = BackgroundScheduler(daemon=True)
+task_scheduler.add_job(check_disaster, 'interval', seconds=5)
+task_scheduler.start()
 
 # Flask object creation
 app = Flask(__name__)
@@ -21,6 +42,7 @@ def index():
 @app.route("/query", methods = ["GET"])
 
 def query():
+	
 	location = request.args.get('location').lower()
 	
 	temperature_url = 'https://www.wolframcloud.com/objects/2fd9ef4c-bad7-4a1e-911b-1612268870a2?a=' + location
@@ -41,18 +63,18 @@ def query():
 	visibility_response = float(visibility_response.text.split(" ")[0])
 	wind_response = float(wind_response.text.split(" ")[0])
 	
+	#forecase_temp_url = 'https://www.wolframcloud.com/objects/2383eee0-e0b4-4eb5-b349-af0eda7ca348?f=' + location
+	
 	return_value = {"temperature": temp_response, "pressure": pressure_response, "humidity": humidity_response, "visibility": visibility_response, "wind": wind_response}
 	json_string = json.dumps(return_value)
 	
 	return json_string
-	
-	#temp_url = 'https://www.wolframcloud.com/objects/d50c639f-5ea7-495b-82ff-48ba3e53dd61?f=' + location
 
 # Error page
 @app.errorhandler(404)
 
 def page_not_found(e):
-	return json.dumps(data_repository["error"]), 404
+	return json.dumps({"message":"The required page was not found."}), 404
 
 if __name__ == "__main__":
 	app.debug = True
