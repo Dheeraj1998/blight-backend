8e664a3ff870a450fae630645ecd7233282b82ae
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2018-03-19 04:15:10 +0530

    Added functionalities for getting forecast data about temperature and weather.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def check_disaster():
 	
 # Starting the scheduler with the required job
 task_scheduler = BackgroundScheduler(daemon=True)
-task_scheduler.add_job(check_disaster, 'interval', seconds=5)
+task_scheduler.add_job(check_disaster, 'interval', minutes=10)
 task_scheduler.start()
 
 # Flask object creation
@@ -50,7 +50,7 @@ def query():
 	humidity_url = 'https://www.wolframcloud.com/objects/118560e8-ed93-4dcc-8ae5-4ebef9e92269?c=' + location
 	visibility_url = 'https://www.wolframcloud.com/objects/23efd16a-2010-476e-ae56-6a756f6d1a73?d=' + location
 	windspeed_url = 'https://www.wolframcloud.com/objects/adb4d8d1-c5d4-4ace-adb9-9379e25ed252?e=' + location
-	
+
 	temp_response = requests.get(temperature_url)
 	pressure_response = requests.get(pressure_url)
 	humidity_response = requests.get(humidity_url)
@@ -63,9 +63,31 @@ def query():
 	visibility_response = float(visibility_response.text.split(" ")[0])
 	wind_response = float(wind_response.text.split(" ")[0])
 	
-	#forecase_temp_url = 'https://www.wolframcloud.com/objects/2383eee0-e0b4-4eb5-b349-af0eda7ca348?f=' + location
+	# Extracing info from 
+	forecast_temp_url = 'https://www.wolframcloud.com/objects/2383eee0-e0b4-4eb5-b349-af0eda7ca348?f=' + location
+	forecast_pressure_url = 'https://www.wolframcloud.com/objects/bdf58b84-08bf-4cb5-9c78-6e647a8eac8f?g=' + location
 	
-	return_value = {"temperature": temp_response, "pressure": pressure_response, "humidity": humidity_response, "visibility": visibility_response, "wind": wind_response}
+	forecast_temp_response = requests.get(forecast_temp_url).text[111:].split("}")[0]
+	forecast_temp_response = forecast_temp_response.split(",")
+	
+	for index in range(0, len(forecast_temp_response)):
+		forecast_temp_response[index] = float(forecast_temp_response[index])
+	
+	forecast_pressure_response = requests.get(forecast_pressure_url).text[111:].split("}")[0]
+	forecast_pressure_response = forecast_pressure_response.split(",")
+	
+	for index in range(0, len(forecast_pressure_response)):
+		forecast_pressure_response[index] = float(forecast_pressure_response[index])
+	
+	return_value = {"temperature": "%.2f" % temp_response, "pressure": "%.2f" % pressure_response, 
+					"humidity": "%.2f" % humidity_response, "visibility": "%.2f" % visibility_response, "wind": "%.2f" % wind_response, 
+					"forecast_t_d1": "%.2f" % forecast_temp_response[0], "forecast_t_d2": "%.2f" % forecast_temp_response[1], 
+					"forecast_t_d3": "%.2f" % forecast_temp_response[2], "forecast_t_d4": "%.2f" % forecast_temp_response[3], 
+					"forecast_t_d5": "%.2f" % forecast_temp_response[4], "forecast_t_d6": "%.2f" % forecast_temp_response[5],
+					"forecast_p_d1": "%.2f" % forecast_pressure_response[0], "forecast_p_d2": "%.2f" % forecast_pressure_response[1], 
+					"forecast_p_d3": "%.2f" % forecast_pressure_response[2], "forecast_p_d4": "%.2f" % forecast_pressure_response[3], 
+					"forecast_p_d5": "%.2f" % forecast_pressure_response[4], "forecast_p_d6": "%.2f" % forecast_pressure_response[5]}
+	
 	json_string = json.dumps(return_value)
 	
 	return json_string
